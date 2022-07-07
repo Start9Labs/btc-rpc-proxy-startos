9deb9320bbd3e4b79c221d15be7310ee12153710
Aiden McClelland <me@drbonez.dev>
2022-07-07 14:44:04 -0600

    fix btc-rpc-proxy migration

--- migrations/from_lt_0_3_2_2.sh
+++ migrations/from_lt_0_3_2_2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-yq e -i '.bitcoind = {"user":null,"password":null}' /root/start9/config.yaml
-echo -n '{"configured":false}'
+yq e -i 'del(.bitcoind)' /root/start9/config.yaml
+echo -n '{"configured":true}'
