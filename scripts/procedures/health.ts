61f94e30b1cbe3a09e658c26110ef2ea66004999
Stephen Chavez <stephen.chavez12@gmail.com>
2022-11-19 12:09:52 -0700

    convert to use new 3.3 api features

---
+++ scripts/procedures/health.ts
@@ -0,0 +1,22 @@
+import { healthUtil, types as T } from "../deps.ts";
+
+export const health: T.ExpectedExports.health = {
+  async "rpc"(effects, duration) {
+    try {
+      await healthUtil.guardDurationAboveMinimum({
+        duration: duration,
+        minimumTime: 5000,
+      });
+    } catch (e) {
+      return e;
+    }
+    const result = await effects.runCommand({
+      command: "check-rpc.sh",
+      args: [],
+    });
+    if ("result" in result) {
+      return { result: null };
+    }
+    return result;
+  },
+};
